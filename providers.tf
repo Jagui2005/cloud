dba9030befa6dbf9b0feebc7546736a8a9652f97
Jagui2005 <juanmiguel301205@gmail.com>
2026-03-03 12:24:47 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -12,7 +12,6 @@ terraform {
 provider "azurerm" {
   features {
     resource_group {
-      # Esto soluciona el error de "Resource Group still contains Resources"
       prevent_deletion_if_contains_resources = false
     }
   }
