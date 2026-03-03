00f25fa4be763fe7e7d771cd7ec882f89a90aa53
Jagui2005 <juanmiguel301205@gmail.com>
2026-03-03 12:25:12 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,3 @@
-# Outputs para AWS
 output "aws_public_ip" {
   value       = aws_instance.awsvm1.public_ip
   description = "IP pública de la instancia AWS"
@@ -9,7 +8,6 @@ output "aws_private_ip" {
   description = "IP privada de la instancia AWS"
 }
 
-# Outputs para Azure
 output "azure_public_ip" {
   value       = azurerm_public_ip.publicip.ip_address
   description = "IP pública de la VM en Azure"
